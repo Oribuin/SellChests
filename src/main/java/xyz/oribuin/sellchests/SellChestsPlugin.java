f78aacded6d65929a1e3c5ceb854b53b7a4221ea
Oribuin <rjmullineux@gmail.com>
2021-02-21 10:32:52 +0000

    Add ShopGUI+ Support

--- src/main/java/xyz/oribuin/sellchests/SellChestsPlugin.java
+++ src/main/java/xyz/oribuin/sellchests/SellChestsPlugin.java
@@ -1,6 +1,8 @@
 package xyz.oribuin.sellchests;
 
+import net.brcdev.shopgui.ShopGuiPlugin;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import xyz.oribuin.orilibrary.OriPlugin;
 import xyz.oribuin.sellchests.command.SellchestsCommand;
 import xyz.oribuin.sellchests.listener.ChestInteract;
@@ -14,13 +16,11 @@ public class SellChestsPlugin extends OriPlugin {
     public void enablePlugin() {
 
         // Detect Holographic Displays
-        if (this.getServer().getPluginManager().getPlugin("HolographicDisplays") == null) {
-            this.getLogger().severe("Please install HolographicDisplays to use this plugin.");
-            this.getLogger().severe("Disabling...");
-            this.getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
+        if (!this.checkHolograms()) return;
 
+        // Detect ShopGUI+
+        if (!this.checkShopGUI()) return;
+        
         // Load Managers asynchronously
         this.getServer().getScheduler().runTaskAsynchronously(this, () -> {
             this.getManager(DataManager.class);
@@ -40,5 +40,39 @@ public class SellChestsPlugin extends OriPlugin {
     @Override
     public void disablePlugin() {
         // Super's disablePlugin
+    }
+
+    /**
+     * Check if the server has HolographicDisplays Enabled.
+     *
+     * @return True if holograms are enabled.
+     */
+    private boolean checkHolograms() {
+        Plugin plugin = this.getServer().getPluginManager().getPlugin("HolographicDisplays");
+        if (plugin == null || !plugin.isEnabled()) {
+            this.getLogger().severe("Please install HolographicDisplays to use this plugin.");
+            this.getLogger().severe("Disabling...");
+            this.getServer().getPluginManager().disablePlugin(this);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if the server has ShopGUI+ Enabled.
+     *
+     * @return True if ShopGUI+ Is enabled.
+     */
+    private boolean checkShopGUI() {
+        Plugin plugin = this.getServer().getPluginManager().getPlugin("ShopGUIPlus");
+        if (plugin == null || !plugin.isEnabled()) {
+            this.getLogger().severe("Please install ShopGUIPlus to use this plugin.");
+            this.getLogger().severe("Disabling...");
+            this.getServer().getPluginManager().disablePlugin(this);
+            return false;
+        }
+
+        return true;
     }
 }
